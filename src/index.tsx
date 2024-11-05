0310b814843ccbc1b1b3332177863a6efe4a9bbd
Nikita <gerasimenya.05@mail.ru>
2024-11-05 19:11:43 +0300

    repair 3

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const element = document.getElementById('root') as HTMLElement;
 
 const root = ReactDOM.createRoot(element);
 root.render(
     <BrowserRouter>
-        <App />
+        <HashRouter>
+            <App />
+        </HashRouter>
     </BrowserRouter>
 );
